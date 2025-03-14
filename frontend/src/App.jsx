4ae2ca97acf8819cca9408f1b2e67c795e1b5169
prateek-balani <prateekbalani03@gmail.com>
2025-03-13 23:43:21 -0700

    bug fix: downloading missing file and changed path s to fix path error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./landing.jsx";
-import Login from "./login.jsx";
-import Register from "./register.jsx";
-import Appointments from "./appointments.jsx";
+import Landing from "./components/landing.jsx";
+import Login from "./components/login.jsx";
+import Register from "./components/register.jsx";
+import Appointments from "./components/appointments.jsx";
 
 const App = () => {
   return (
