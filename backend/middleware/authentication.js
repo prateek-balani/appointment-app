c808f4673e408c23a2e9ea4dc30319c4809a9a78
prateek-balani <prateekbalani03@gmail.com>
2025-03-13 22:56:46 -0700

    feature: configured middleware and created empty frontend pages

---
+++ backend/middleware/authentication.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const secretKey = process.env.SECRET_KEY
+
+// Middleware to verify admin user based on JWT token
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Extract token from header
+
+  if (!token) return res.status(401).json({ message: "Access denied, no token provided." });
+
+  try {
+    // Verify token with secret key
+    const decoded = jwt.verify(token, secretKey);
+
+    if (decoded.role !== "admin" && decoded.role !== "staff" && decoded.role !== "applicant") {
+      return res.status(403).json({ message: "error user login related" });
+    }
+
+    req.user = decoded; // Attach user info to request
+    next(); // Proceed to next middleware or route handler
+  } catch (error) {
+    res.status(403).json({ message: "Invalid or expired token." });
+  }
+};
+
+module.exports = verifyToken;
