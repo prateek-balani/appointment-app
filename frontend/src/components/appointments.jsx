5575f7930acf6c2a7b0adcd234311fc404789c88
prateek-balani <prateekbalani03@gmail.com>
2025-03-19 15:14:37 -0700

    feature: updated ui for appointments page

--- frontend/src/components/appointments.jsx
+++ frontend/src/components/appointments.jsx
@@ -6,44 +6,50 @@ const isProduction = process.env.NODE_ENV === "production";
 
 
 const Appointments = () => {
-    const [appointments, setAppointments] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
-    useEffect(() => {
-        const getAppointments = async () => {
-            try {
-                fetch(isProduction ? "/api/appointments":"http://localhost:5050/api/appointments")
-                    .then((res) => res.json())
-                    .then((data) => {
-                        setAppointments(data);
-                    }
-                    );
-            } catch (e) {
-                console.error(e.message);
-            }
-        };
-        getAppointments();
-    }, []);
-    
-    if (appointments.length === 0) {
-    return <p>No appointments found.</p>;
+  useEffect(() => {
+    const getAppointments = async () => {
+      try {
+        fetch(isProduction ? "/api/appointments" : "http://localhost:5050/api/appointments")
+          .then((res) => res.json())
+          .then((data) => {
+            setAppointments(data);
+          }
+          );
+      } catch (e) {
+        console.error(e.message);
+      }
+    };
+    getAppointments();
+  }, []);
+
+  if (appointments.length === 0) {
+    return <section className="flex items-center justify-center h-screen bg-gray-700">
+      <div className="w-full max-w-md p-8 space-y-5 bg-black rounded-lg shadow-lg dark:bg-gray-800">
+        <p className="text-base font-semibold text-center text-gray-900 dark:text-white">No appointments found.</p>
+      </div>
+    </section>;
   }
 
   return (
-    <div>
-      <h1>Appointments</h1>
-      <ul>
-        {appointments.map((appt) => (
-          <li key={appt.id}>
-            <p>ID: {appt.id}</p>
-            <p>Date/Time: {appt.dateTime}</p>
-            <p>Details: {appt.details}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section className="flex items-center justify-center h-screen bg-gray-700">
+      <div className="w-full max-w-md p-8 space-y-5 bg-black rounded-lg shadow-lg dark:bg-gray-800">
+        <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-white">Appointments</h1>
+        <ul>
+          {appointments.map((appt) => (
+            <li key={appt.id}>
+              <p className="text-base font-semibold text-center text-gray-900 dark:text-white">ID: {appt.id}</p>
+              <p className="text-base font-semibold text-center text-gray-900 dark:text-white">Date/Time: {appt.dateTime}</p>
+              <p className="text-base font-semibold text-center text-gray-900 dark:text-white">Details: {appt.details}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
-    
+
 
 
 export default Appointments;
