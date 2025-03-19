30ca1a0166ae2e976088efa155017460cb0e673b
prateek-balani <prateekbalani03@gmail.com>
2025-03-19 14:32:40 -0700

    feature: updated ui for register.jsx

--- frontend/src/components/register.jsx
+++ frontend/src/components/register.jsx
@@ -16,15 +16,15 @@ const Register = () => {
 
     const registerUser = async (e) => {
         e.preventDefault();
-      
+
         try {
-            const response = await fetch(isProduction?"/api/users/register" :"http://localhost:5050/api/users/register", { // hardcoding it to backend register route 
+            const response = await fetch(isProduction ? "/api/users/register" : "http://localhost:5050/api/users/register", { // hardcoding it to backend register route 
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ fname, lname, email, pass, role }),
             });
 
-            
+
             if (response.ok) {
                 const user = await response.json();
                 console.log(user);
@@ -42,26 +42,50 @@ const Register = () => {
         }
     }
     return (
-        <div>
-            <h1>Register</h1>
-            <form onSubmit={registerUser}>  {/*form to register user */}
-                <input placeholder="First Name" value={fname} onChange={(e) => setFname(e.target.value)} />
-                <input placeholder="Last Name" value={lname} onChange={(e) => setLname(e.target.value)} />
-                <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input
-                  type="password"
-                  placeholder="Password"
-                  value={pass}
-                  onChange={(e) => setPass(e.target.value)}
-                />
-                <select placeholder="Role" value={role} onChange={(e) => setRole(e.target.value)}>
-                    <option value="admin">Admin</option>
-                    <option value="applicant">applicant</option>
-                    <option value="staff">Staff</option>
-                </select>
-                <button type="submit">Register</button>
-            </form>
-        </div>
+        <section class="flex flex-col items-center justify-center h-screen bg-black">
+            <div class="w-full max-w-md p-8 space-y-5 bg-black rounded-lg shadow-lg dark:bg-gray-800">
+                <h1 class="text-2xl font-semibold text-center text-gray-900 dark:text-white">Welcome to the Registration Page</h1>
+
+                <form onSubmit={registerUser}>  {/*form to register user */}
+              
+                    <div class="space-y-4">
+
+                        <div>
+                            <label for="fname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                            <input placeholder="First Name" id="fname" value={fname} onChange={(e) => setFname(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                        </div>
+                        <div>
+                            <label for="lname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                            <input placeholder="Last Name" id="lname" value={lname} onChange={(e) => setLname(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input placeholder="Email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                value={pass}
+                                onChange={(e) => setPass(e.target.value)}
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            />
+                        </div>
+                        <div>
+                            <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                            <select placeholder="Role" id="role" value={role} onChange={(e) => setRole(e.target.value)} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option value="admin">Admin</option>
+                                <option value="applicant">Applicant</option>
+                                <option value="staff">Staff</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 }
 export default Register;
