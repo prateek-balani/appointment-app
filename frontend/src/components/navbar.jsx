9b720eb96d12e33227083a409847d827be58749c
prateek-balani <prateekbalani03@gmail.com>
2025-08-08 15:42:10 -0700

    fixed css issues with navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <>
-            <div className='navbar bg-white shadow-sm'>
+            <div className='navbar bg-gray-900 shadow-sm'>
                 <div className='navbar-start'>
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
                         </div>
                         <ul
                             tabIndex={0}
@@ -20,7 +20,7 @@ const Navbar = () => {
 
                 </div>
                 <div className="navbar-center">
-                    <Link to='/' className="btn btn-ghost text-xl">Appointment App</Link>
+                    <Link to='/' className="btn btn-ghost text-xl text-white">Appointment App</Link>
                 </div>
                 <div className="navbar-end">
                     <div className='space-x-1'>
