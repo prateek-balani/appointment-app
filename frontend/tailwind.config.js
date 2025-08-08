069ee1feeb386c947e74af13684335ab86f1f926
prateek-balani <prateekbalani03@gmail.com>
2025-08-08 15:15:26 -0700

    configured daisyui for easy component building

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import daisyui from 'daisyui'
+
+export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
