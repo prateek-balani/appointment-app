2a21861fc1b47b21af46a2ea53ed56bf38354871
prateek-balani <prateekbalani03@gmail.com>
2025-08-11 10:52:35 -0700

    added delete appointment functionality

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -31,16 +31,16 @@ router.get("/today", async (req, res) => {
 
 // list available appointments
 router.get("/appt", async (req, res) => {
-  const {date} = req.query ;
+  const { date } = req.query;
 
   try {
-    const results = await req.db.all("SELECT * FROM appointments WHERE DATE(dateTime) = ?",[date]);
+    const results = await req.db.all("SELECT * FROM appointments WHERE DATE(dateTime) = ?", [date]);
 
     const hours = [
-      "9:00", "10:00","11:00","12:00","13:00", "14:00", "15:00", "16:00"
+      "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"
     ];
 
-    const timeSlots = results.map(slots => slots.dateTime.slice(11,16));
+    const timeSlots = results.map(slots => slots.dateTime.slice(11, 16));
 
     const availableSlots = hours.filter(time => !timeSlots.includes(time));
 
@@ -57,7 +57,7 @@ router.get("/appt", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-     const appointmentId = req.params.id;
+    const appointmentId = req.params.id;
     const results = await req.db.all("SELECT * FROM appointments WHERE id = ?", appointmentId);
     res.status(200).send(results);
   } catch (e) {
@@ -68,57 +68,60 @@ router.get("/:id", async (req, res) => {
 
 // create a new appointment
 router.post("/", async (req, res) => {
-    try {
-        const {createdById, assignedToId, dateTime, details} = req.body;
-        const query = "INSERT INTO appointments (createdById, assignedToId, dateTime, details) VALUES (?, ?, ?, ?)";
-        const values = [createdById, assignedToId, dateTime, details];
+  try {
+    const { createdById, assignedToId, dateTime, details } = req.body;
+    const query = "INSERT INTO appointments (createdById, assignedToId, dateTime, details) VALUES (?, ?, ?, ?)";
+    const values = [createdById, assignedToId, dateTime, details];
 
-        const {lastID} = await req.db.run(query, values);
-        res.status(201).send(`appointment created with id: ${lastID}`);
-    } catch (e) {
-        console.error("error creating appointment:", e);
-        res.status(500).json({error: e.message});
-    }
+    const { lastID } = await req.db.run(query, values);
+    res.status(201).send(`appointment created with id: ${lastID}`);
+  } catch (e) {
+    console.error("error creating appointment:", e);
+    res.status(500).json({ error: e.message });
+  }
 }
 );
 
 
 // update an appointment
 
-router.put("/:id",verifyToken, async (req, res) => {
-    try {
-        const appointmentId = req.params.id;
-        const {createdById, assignedToId, dateTime, details} = req.body;
-        const query = "UPDATE appointments SET createdById = ?, assignedToId = ?, dateTime = ?, details = ? WHERE id = ?";
-        const values = [createdById, assignedToId, dateTime, details, appointmentId];
+router.put("/:id", verifyToken, async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    const { createdById, assignedToId, dateTime, details } = req.body;
+    const query = "UPDATE appointments SET createdById = ?, assignedToId = ?, dateTime = ?, details = ? WHERE id = ?";
+    const values = [createdById, assignedToId, dateTime, details, appointmentId];
 
 
-        const { lastID } = await req.db.run(query, values);
-        res.status(200).send(`appointment updated with id: ${lastID}`);
-    }
-       
-     catch (e) {
-        console.error("error updating appointment:", e);
-        res.status(500).json({error: e.message});
-    }
+    const { lastID } = await req.db.run(query, values);
+    res.status(200).send(`appointment updated with id: ${lastID}`);
+  }
+
+  catch (e) {
+    console.error("error updating appointment:", e);
+    res.status(500).json({ error: e.message });
+  }
 });
 
 // delete an appointment
 
-router.delete("/:id",verifyToken, async (req, res) => {
-    try{
-        const appointmentId = req.params.id;
-        const query = "DELETE FROM appointments WHERE id = ?";
-        const values = [appointmentId];
+router.delete("/:id", async (req, res) => {
+  try {
+    const appointmentId = req.params.id;
+    const query = "DELETE FROM appointments WHERE id = ?";
+    const values = [appointmentId];
 
-        const { lastID } = await req.db.run(query, values);
-        res.status(200).send(`appointment updated with id: ${lastID}`);
-        }
-    
-    catch(e) {
-        console.error("error deleting appointment:", e);
-        res.status(500).json({error: e.message});
+    const result = await req.db.run(query, values);
+    if (result.changes === 0) {
+      return res.status(404).json({ error: "Appointment not found" });
     }
+    res.status(200).send(`appointment deleted ${appointmentId}`);
+  }
+
+  catch (e) {
+    console.error("error deleting appointment:", e);
+    res.status(500).json({ error: e.message });
+  }
 });
 
 module.exports = router;
--- frontend/src/pages/appointments.jsx
+++ frontend/src/pages/appointments.jsx
@@ -22,7 +22,7 @@ const Appointments = () => {
 
       }
       try {
-       
+
         const response = await fetch(isProduction ? `/api/appointments/${userId}` : `http://localhost:5050/api/appointments/${userId}`, {
           headers: {
             "Content-Type": "application/json",
@@ -53,20 +53,53 @@ const Appointments = () => {
     </section>;
   }
 
+  const deleteAppointment = async (id) => {
+    try {
+      const response = await fetch(
+        isProduction
+          ? `/api/appointments/${id}`
+          : `http://localhost:5050/api/appointments/${id}`,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Failed to delete appointment: ${response.statusText}`);
+      }
+      window.location.reload();
+
+    } catch (e) {
+      console.error(e.message);
+      alert("Failed to delete appointment. Try again.");
+    }
+
+
+  }
+
   return (
     <section className="flex items-center justify-center h-screen bg-gray-700">
       <div className="w-full max-w-md p-8 space-y-5 bg-black rounded-lg shadow-lg dark:bg-gray-800">
         <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-white">Appointments</h1>
-        <ul>
-          {appointments.map((appt) => (
+
+        {appointments.map((appt) => (
+          <ul>
             <li key={appt.id}>
               <p className="text-base font-semibold text-center text-gray-900 dark:text-white">ID: {appt.id}</p>
               <p className="text-base font-semibold text-center text-gray-900 dark:text-white">Date/Time: {appt.dateTime}</p>
               <p className="text-base font-semibold text-center text-gray-900 dark:text-white">Details: {appt.details}</p>
             </li>
-            
-          ))}
-        </ul>
+            <button
+              onClick={() => deleteAppointment(appt.id)}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Cancel Appointment
+            </button>
+          </ul>
+        ))}
+
       </div>
     </section>
   );
